4e89f3684b82afb65d5d6da6f960c7e31d35ceac
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-07-18 19:26:31 -0400

    added new nav links

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,36 +1,20 @@
 
     import React from 'react';
-    import { NavLink } from 'react-router-dom';
-    import { useSelector } from 'react-redux';
-    import ProfileButton from './ProfileButton';
     //import './Navigation.css';
 
     function Navigation({ isLoaded }){
-      const sessionUser = useSelector(state => state.session.user);
-
-      let sessionLinks;
-      if (sessionUser) {
-        sessionLinks = (
-          <li>
-            <ProfileButton user={sessionUser} />
-          </li>
-        );
-      } else {
-        sessionLinks = (
-          <li>
-            <NavLink to="/login">Log In</NavLink>
-            <NavLink to="/signup">Sign Up</NavLink>
-          </li>
-        );
-      }
 
       return (
-        <ul>
-          <li>
-            <NavLink exact to="/">Home</NavLink>
-          </li>
-          {isLoaded && sessionLinks}
-        </ul>
+        <header>
+          <div>MTK Logo</div>
+          <ul>
+            <li>Home</li>
+            <li>Our Roots</li>
+            <li>Products</li>
+            <li>Contact Us</li>
+            <li>Order Online!</li>
+          </ul>
+        </header>
       );
     }
 
