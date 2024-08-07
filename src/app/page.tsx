cc6173d3360a38b0e349dce59c09a805b3654569
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-08-07 00:03:17 -0400

    init shadcdn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import { SessionProvider } from 'next-auth/react'
 import Link from "next/link";
 import { Products } from "./_components/products";
 import SignInAndOut from "./_components/signInAndOut"
+import Navbar from './_components/Navbar';
 export default function HomePage() {
   return (
 
     <SessionProvider>
+      <Navbar />
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
