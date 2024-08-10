9f65f23076bd20213ea826ce1904d241347d84a7
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-08-10 11:37:28 -0400

    removed boiler code

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from '../../components/ui/button'
+import SignInAndOut from './signInAndOut';
 const Navbar = () => {
   return (
     <nav>
@@ -16,7 +17,7 @@ const Navbar = () => {
             <a className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium">Contact</a>
           </Button>
         </div>
-        <Button className="ml-4">Sign In</Button>
+        <SignInAndOut />
       </div>
     </nav>
   );
