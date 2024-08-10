db086b33ca4fcd92201b677c7d5d3ac2e97f843e
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-08-10 11:45:06 -0400

    set up logo placement

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -4,6 +4,9 @@ import SignInAndOut from './signInAndOut';
 const Navbar = () => {
   return (
     <nav>
+      <div className='flex justify-center items-center h-32 w-32 my-0 mt-4 rounded mx-auto bg-red-400'>
+        logo
+      </div>
       <div className="mx-auto px-4 py-2 flex justify-between items-center">
         <a className="text-lg font-bold text-gray-900">MyApp</a>
         <div>
