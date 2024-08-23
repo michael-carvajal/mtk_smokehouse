b8dc21920596b4c514857204d98e7bbdf525c009
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-08-23 17:32:11 -0400

    added images and logo

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Button } from '../../components/ui/button'
 import Link from 'next/link';
+import Image from 'next/image';
 const Navbar = () => {
   return (
     <nav>
-      <Link href="/" className='flex justify-center items-center h-32 w-32 my-0 mt-4 rounded mx-auto bg-red-400'>
-        logo
+      <Link href="/" className='flex justify-center items-center h-32 w-32 my-0 mt-4 rounded mx-auto '>
+        <Image  alt='company logo' src='/mtk_photos/logo.jpg' width="128" height="128"/>
       </Link>
       <div className="mx-auto px-4 py-2 md:flex justify-between items-center flex-wrap hidden">
           <Button variant='ghost' className='text-slate-600 tracking-wider font-thin'>
