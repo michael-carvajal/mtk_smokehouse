4f50a44dc16bbcd21330a0379a881aab1a6211bb
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-08-25 14:26:23 -0400

    added dashboard and dynamically remove the main nav bar

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -1,10 +1,15 @@
+'use client'
 import React from 'react';
 import { Button } from '../../components/ui/button'
 import Link from 'next/link';
 import Image from 'next/image';
+import {usePathname} from 'next/navigation'
 const Navbar = () => {
+  const pathname = usePathname()
+  console.log(pathname);
+  
   return (
-    <nav>
+    <nav className={`${pathname === "/dashboard" && "hidden"} `}>
       <Link href="/" className='flex justify-center items-center h-32 w-32 my-0 mt-4 rounded mx-auto '>
         <Image  alt='company logo' src='/mtk_photos/logo.jpg' width="128" height="128"/>
       </Link>
