9a69ceb3c26f6f1a25f781f8694c665bc954bd4f
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-09-03 21:34:49 -0400

    fixed nav bar href link

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   
   return (
     <nav>
-      <Link href="/home" className='flex justify-center items-center h-32 w-32 my-0 mt-4 rounded mx-auto '>
+      <Link href="/" className='flex justify-center items-center h-32 w-32 my-0 mt-4 rounded mx-auto '>
         <Image  alt='company logo' src='/mtk_photos/logo.jpg' width="128" height="128"/>
       </Link>
       <div className="mx-auto px-4 py-2 md:flex justify-between items-center flex-wrap hidden">
@@ -18,12 +18,12 @@ const Navbar = () => {
           <Button variant='ghost' className='text-slate-600 tracking-wider font-thin'>
             Our Roots
           </Button>
-          <Link href="/home/contact">
+          <Link href="/contact">
             <Button variant='ghost' className='text-slate-600 tracking-wider font-thin'>
               Contact / Locations
             </Button>
           </Link>
-          <Link href='/home/products'>
+          <Link href='/products'>
             <Button variant='ghost' className='text-slate-600 tracking-wider font-thin'>
               Order Online
             </Button>
