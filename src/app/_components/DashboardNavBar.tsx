206b54c1ff54ff52641c9f68ae0ff91366e777c7
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-09-08 21:37:15 -0400

    moved dahsboard drop down to right

--- src/app/_components/DashboardNavBar.tsx
+++ src/app/_components/DashboardNavBar.tsx
@@ -49,17 +49,7 @@ function DashboardNavBar() {
                     </nav>
                 </SheetContent>
             </Sheet>
-            <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-                <form className="ml-auto flex-1 sm:flex-initial">
-                    <div className="relative">
-                        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                        <Input
-                            type="search"
-                            placeholder="Search products..."
-                            className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-                        />
-                    </div>
-                </form>
+            <div className="flex w-full items-center justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4">
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
                         <Button variant="secondary" size="icon" className="rounded-full">
