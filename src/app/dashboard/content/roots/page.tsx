00d6260220eeab6783124a710ff97b4adb8562c1
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-09-11 22:04:18 -0400

    able to edit image of ou roots

--- src/app/dashboard/content/roots/page.tsx
+++ src/app/dashboard/content/roots/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 import { useQuery } from "@tanstack/react-query";
-import { Edit } from "lucide-react";
+import { Edit, Upload } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { Input } from "~/components/ui/input";
 import { Textarea } from "~/components/ui/textarea";
 import { api } from "~/utils/api";
+import UploadImage from "../homePage/UploadImage";
 
 export default function OurRoots() {
   const [pageState, setPageState] = useState({});
@@ -27,31 +28,34 @@ export default function OurRoots() {
     }
   }, [rootsPage]);
 
+  const setFeatureImageLink = (link) => {
+    setPageState({ ...pageState, imageUrl: link })
+  }
   return (
     <Card className="max-w-xl mx-auto relative">
       <Edit onClick={() => setIsEditing(!isEditing)} className="absolute -left-20 cursor-pointer text-slate-900" />
       <CardContent className="p-0">
         <div className="relative aspect-[4/3] w-full md:h-[300px]">
-          <Image
+          {isEditing ? <UploadImage setFeatureImageLink={setFeatureImageLink} /> : <Image
             src={pageState.imageUrl}
             alt="Misty forest landscape with evergreen trees"
             fill
             className="object-cover rounded-t-lg"
-          />
+          />}
         </div>
       </CardContent>
       <CardHeader>
         <CardTitle className="text-2xl font-extralight text-center mb-4">{isEditing ? (
-            <Input
-              value={pageState.title}
-              onChange={(e) =>
-                setPageState({ ...pageState, title: e.target.value })
-              }
-              className="text-center"
-            />
-          ) : (
-            pageState.title
-          )}</CardTitle>
+          <Input
+            value={pageState.title}
+            onChange={(e) =>
+              setPageState({ ...pageState, title: e.target.value })
+            }
+            className="text-center"
+          />
+        ) : (
+          pageState.title
+        )}</CardTitle>
       </CardHeader>
       <CardContent className="text-center px-20 pb-6">
         <p className="text-sm leading-relaxed text-gray-600">
