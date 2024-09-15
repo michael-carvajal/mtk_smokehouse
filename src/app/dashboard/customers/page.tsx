65d4787f085fe0a7e4f52e183e582e8d22922d3c
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-09-15 09:33:05 -0400

    placed boiler pagers for customers and orders

---
+++ src/app/dashboard/customers/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function page() {
+    return (
+        <div className="h-screen flex">
+            <div className='flex flex-col  items-center  justify-center flex-1 gap-7 max-w-72 relative'>
+                <p className="text-2xl text-slate-800">Page coming soon.</p>
+            </div>
+        </div>
+    )
+}
+
+export default page
