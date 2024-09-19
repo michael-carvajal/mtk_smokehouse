9b5008056b40e9c3926b7ec83f59d0b2102f6a59
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-09-19 18:24:02 -0400

    added headers for product dashboard page

--- src/app/dashboard/products/[id]/page.tsx
+++ src/app/dashboard/products/[id]/page.tsx
@@ -57,7 +57,7 @@ function ProductDetails() {
       return response
     } catch (error) {
       console.log('cresation error', error);
-      
+
     }
   }
   const mutationProductDeletion = useMutation({
@@ -73,58 +73,61 @@ function ProductDetails() {
     setPageState({ ...pageState, imageLink: link })
   }
   return (
+    <div className="flex flex-col gap-4 items-center">
+      <h1 className="text-2xl font-bold text-slate-900">Edit the {product.name} product details</h1>
 
-    <Card className="md:w-[400px] max-w-sm mx-auto relative">
-      <Edit onClick={() => setIsEditing(!isEditing)} className="absolute -left-20 cursor-pointer text-slate-900" />
-      <Trash onClick={() => mutationProductDeletion.mutate()} className="absolute -left-20 top-20 cursor-pointer text-red-500" />
-      <CardHeader>
-        <CardTitle className="text-lg">{isEditing ? (
-          <Input
-            value={pageState.name}
-            onChange={(e) =>
-              setPageState({ ...pageState, name: e.target.value })
-            }
-          />
-        ) : (pageState.name)}</CardTitle>
-      </CardHeader>
-      <CardContent className="p-0">
-        <div className="relative aspect-[4/3] w-full">
-          {isEditing ? <UploadImage setFeatureImageLink={setFeatureImageLink} /> : <Image
-            src={pageState.imageLink || '/mtk_photos/Image-not-found.webp'}
-            alt={"pageState image"}
-            fill
-            className="object-cover rounded-t-lg"
-          />}
-        </div>
-        <div className="p-4">
-          <p className="text-2xl font-bold mb-2">{isEditing ? (
-          <Input
-            value={pageState.price}
-            onChange={(e) =>
-              setPageState({ ...pageState, price: e.target.value })
-            }
-          />
-        ) : (pageState.price)}</p>
-          <p className="text-sm text-gray-600">{isEditing ? (
-          <Input
-            value={pageState.description}
-            onChange={(e) =>
-              setPageState({ ...pageState, description: e.target.value })
-            }
-          />
-        ) : (pageState.description)}</p>
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full">Add to Cart</Button>
-      </CardFooter>
-      <CardFooter className="border-t px-6 py-4">
-        <Button onClick={() => {
-          mutationProductUpdate.mutate(pageState)
-        }}>Save</Button>
-        {mutationProductUpdate.isError && <div className='text-slate-900 ml-12'>Successfully updated feature</div>}
-      </CardFooter>
-    </Card>
+      <Card className="md:w-[400px] max-w-sm mx-auto relative">
+        <Edit onClick={() => setIsEditing(!isEditing)} className="absolute -left-20 cursor-pointer text-slate-900" />
+        <Trash onClick={() => mutationProductDeletion.mutate()} className="absolute -left-20 top-20 cursor-pointer text-red-500" />
+        <CardHeader>
+          <CardTitle className="text-lg">{isEditing ? (
+            <Input
+              value={pageState.name}
+              onChange={(e) =>
+                setPageState({ ...pageState, name: e.target.value })
+              }
+            />
+          ) : (pageState.name)}</CardTitle>
+        </CardHeader>
+        <CardContent className="p-0">
+          <div className="relative aspect-[4/3] w-full">
+            {isEditing ? <UploadImage setFeatureImageLink={setFeatureImageLink} /> : <Image
+              src={pageState.imageLink || '/mtk_photos/Image-not-found.webp'}
+              alt={"pageState image"}
+              fill
+              className="object-cover rounded-t-lg"
+            />}
+          </div>
+          <div className="p-4">
+            <p className="text-2xl font-bold mb-2">{isEditing ? (
+              <Input
+                value={pageState.price}
+                onChange={(e) =>
+                  setPageState({ ...pageState, price: e.target.value })
+                }
+              />
+            ) : (pageState.price)}</p>
+            <p className="text-sm text-gray-600">{isEditing ? (
+              <Input
+                value={pageState.description}
+                onChange={(e) =>
+                  setPageState({ ...pageState, description: e.target.value })
+                }
+              />
+            ) : (pageState.description)}</p>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Add to Cart</Button>
+        </CardFooter>
+        <CardFooter className="border-t px-6 py-4">
+          <Button onClick={() => {
+            mutationProductUpdate.mutate(pageState)
+          }}>Save</Button>
+          {mutationProductUpdate.isError && <div className='text-slate-900 ml-12'>Successfully updated feature</div>}
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
 
