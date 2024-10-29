418187c76a866e60351e9b36a8de584f1f381de4
michael.carvajal <michaelcarvajal.work@gmail.com>
2024-10-29 15:53:23 -0400

    added suspense boundery for useSearchParams so it could build in production

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,19 +1,17 @@
 'use client'
-import React, { useCallback } from 'react';
+import React, { Suspense, useCallback } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import {
   EmbeddedCheckoutProvider,
   EmbeddedCheckout
 } from '@stripe/react-stripe-js';
-import { useCart } from '~/context/cartContext';
-import { useParams, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 
 const stripePromise = loadStripe("pk_test_51QAuDDAOSV4xdGhW7bX3wpHlxTEkMMLvj7x7KdqEL0I20QRxe3XsboUqFDra5ej5VoCpTgttUYbqySAN36lsahfS00PgYNw92W");
 
-export default function EmbeddedCheckoutComp() {
-const params = useSearchParams();
-const cart = params.get('cart')
-console.log('data--------> ', cart);
+function StripeEmbeddedCheckout() {
+  const params = useSearchParams();
+  const cart = params.get('cart')
   const fetchClientSecret = useCallback(() => {
     // Create a Checkout Session using the cart items
     return fetch("/api/stripe/create-checkout-session", {
@@ -25,13 +23,11 @@ console.log('data--------> ', cart);
     })
       .then((res) => res.json())
       .then((data) => {
-        console.log('data--------> ', data);
-        
         return data.clientSecret
       });
   }, [cart]); // Rerun the function when the cart changes
 
-  
+
   const options = { fetchClientSecret };
 
   return (
@@ -45,3 +41,13 @@ console.log('data--------> ', cart);
     </div>
   );
 }
+
+function CustomEmbeddedCheckout() {
+  return (
+    <Suspense>
+      <StripeEmbeddedCheckout />
+    </Suspense>
+  )
+}
+
+export default CustomEmbeddedCheckout
