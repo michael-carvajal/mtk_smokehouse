b5a412b6c918ca9f8ec0e8dd85d41144684935e8
michael.carvajal <michaelcarvajal.work@gmail.com>
2024-10-30 20:31:29 -0400

    adjusting homepage image

--- src/app/_components/HomePageImage.tsx
+++ src/app/_components/HomePageImage.tsx
@@ -1,21 +1,50 @@
-import React from 'react'
-import Image  from 'next/image'
+import { useState, useEffect, useRef } from 'react';
+import Image from 'next/image';
+import HomePageBGImage from "public/mtk_photos/homePageImage.jpg"
 
-function HomePageImage({scrollY}) {
+function HomePageImage() {
+  const [parallaxOffset, setParallaxOffset] = useState(0);
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          window.addEventListener('scroll', handleScroll);
+        } else {
+          window.removeEventListener('scroll', handleScroll);
+        }
+      },
+      { threshold: 0.1 }
+    );
+
+    observer.observe(imageRef.current);
+
+    function handleScroll() {
+      const offset = window.scrollY * 0.05;
+      setParallaxOffset(offset);
+    }
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      observer.disconnect();
+    };
+  }, []);
+
   const parallaxStyle = {
-    transform: `translateY(${scrollY * 0.1}px)`,
+    transform: `translateY(${parallaxOffset}px)`,
   };
+
   return (
-    <div style={parallaxStyle} className="absolute inset-0  md:block">
-    <Image
-      src="/mtk_photos/homePageImage.jpg"
-      alt="MT Kisco Smokehouse background"
-      layout="fill"
-      objectFit="cover"
-      priority
-    />
-  </div>
-  )
+    <div ref={imageRef} style={parallaxStyle} className="absolute inset-0">
+      <Image
+        src={HomePageBGImage}
+        alt="MT Kisco Smokehouse background"
+        layout="fill"
+        objectFit="cover"
+      />
+    </div>
+  );
 }
 
-export default HomePageImage
+export default HomePageImage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,19 @@
 'use client'
 import { useState, useEffect, useRef } from 'react';
-import throttle from 'lodash.throttle';
 import FeaturedOne from "./_components/FeaturedOne";
 import HomePageImage from './_components/HomePageImage';
 
 export default function HomePage() {
   const [isVisible, setIsVisible] = useState(false);
-  const [scrollY, setScrollY] = useState(0);
-  const imageRef = useRef<HTMLDivElement>(null);
-
-  const handleScroll = throttle(() => {
-    setScrollY(window.scrollY);
-  }, 100); // Adjust the throttle delay as needed
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsVisible(true);
     }, 100);
 
 
-    window.addEventListener('scroll', handleScroll);
 
     return () => {
       clearTimeout(timer);
-      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
@@ -31,15 +22,12 @@ export default function HomePage() {
   return (
     <main className='w-full'>
       <div
-        className={`relative h-[220px]  md:h-screen overflow-hidden transition-opacity duration-1000 ease-in-out ${isVisible ? 'opacity-100' : 'opacity-0'
-          }`}
-        ref={imageRef}
-      >
-        <HomePageImage scrollY={scrollY}/>
+        className={`relative h-[200px]  md:h-[400px] lg:h-[560px] overflow-hidden transition-opacity duration-1000 ease-in-out ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+        <HomePageImage />
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className='text-white flex flex-col gap-8 justify-center items-center flex-1'>
             <h1 className='text-4xl font-bold text-center'>Welcome To MT Kisco Smokehouse</h1>
-            <h3 className='text-xl'>520 Lexington Ave, Mt Kisco, NY 10549</h3>
+            <h3 className='text-xl text-center'>520 Lexington Ave, Mt Kisco, NY 10549</h3>
           </div>
         </div>
       </div>
